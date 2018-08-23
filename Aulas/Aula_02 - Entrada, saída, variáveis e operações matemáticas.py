e7d8a69070f1645bbed7ac628888e79b48036dad
Sandro Lucas <sandro.ferreira@gruporbs.com.br>
2018-08-23 15:18:23 -0300

    Aula 2 - começo

---
+++ Aulas/Aula_02 - Entrada, saída, variáveis e operações matemáticas.py
@@ -0,0 +1,9 @@
+print('Hello Word!\nSegundo print com Quebra linha')
+print('Hello Word!\tSegundo print com Tab\n')
+
+print('#'*3,'Variáveis','#'*3)
+
+nome = 'Sandro Lucas'
+print('Imprimindo nome com variável: {}'.format(nome))
+
+
